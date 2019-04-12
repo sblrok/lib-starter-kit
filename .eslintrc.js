fc22303f9d6e13e3eb2d01c99b64092a305e0477
Igor Suvorov <hi@isuvorov.ru>
2019-04-12 17:29:15 +0300

    фикс ошибок

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 var path = require('path');
 const user = process.env.USER;
+const error = user === 'isuvorov' ? 'off' : 'error';
 const warn = user === 'isuvorov' ? 'off' : 'error';
 
 const res =  {
@@ -24,13 +25,24 @@ const res =  {
     __DEV__: true,
   },
   rules: {
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        ignoreComments: true,
+      }
+    ],   
     'class-methods-use-this': 'off',    
     // 'global-require': 'off',    
     'lines-between-class-members': 'off',    
     'func-names': 'off',    
     'no-underscore-dangle': 'off',    
     'no-throw-literal': 'off',    
-    'react/prop-types': warn,    
+    // 
+    'react/prop-types': error,    
+    'react/forbid-prop-types': error,    
+    // 'react/button-has-type': error,    
+    'object-curly-newline': 'off', // очень странное правило, почитать почему 
   },
   settings: {
     "import/resolver": {
