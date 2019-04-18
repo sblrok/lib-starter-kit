84b2e3e3c3c1b0271a42e93e7dcf8750e4228896
Igor Suvorov <hi@isuvorov.ru>
2019-04-18 20:13:08 +0300

    fix bootstrap

--- scripts/package-bootstrap.sh
+++ scripts/package-bootstrap.sh
@@ -4,7 +4,8 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # pre link
 rm -rf release && \
 rm -rf build && mkdir -p build && \
-cd build && npm link && cd .. \
+
+# cd build && npm link && cd .. \
 
 rm -rf node_modules &&  npm i && mkdir -p node_modules && \
 cp -R package.json build && \
