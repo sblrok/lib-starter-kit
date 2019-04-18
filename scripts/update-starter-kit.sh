6ead97104fdc9baff806a1597e5a62cfd99812d4
Igor Suvorov <hi@isuvorov.ru>
2019-04-18 17:03:05 +0300

    quick fix

--- scripts/update-starter-kit.sh
+++ scripts/update-starter-kit.sh
@@ -2,4 +2,4 @@ HOME='.'
 rsync -avE --progress  ../lib-starter-kit/ $HOME --exclude='*/' --exclude='lerna.json' --exclude='.huskyrc.json' --exclude='README.md'
 rsync -avE --progress  ../lib-starter-kit/.storybook $HOME
 rsync -avE --progress  ../lib-starter-kit/scripts/* $HOME/scripts
-echo "npm install && npm run bootstrap && npm run update"
+echo "\n\n\nYou need:\nnpm install && npm run bootstrap && npm run update"
