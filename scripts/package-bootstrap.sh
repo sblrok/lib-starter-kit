f0ac9c9e2042b259cd8f59edeef10559fe2a7fc1
Igor Suvorov <hi@isuvorov.ru>
2019-08-20 13:09:48 +0400

    рабочие фиксы

--- scripts/package-bootstrap.sh
+++ scripts/package-bootstrap.sh
@@ -8,8 +8,13 @@ rm -rf build && mkdir -p build && \
 rm -rf node_modules && npm i && mkdir -p node_modules && \
 cp -R package.json build && \
 cp -R package-lock.json build && \
-npm run bootstrap:nodemodules && \
-npm run link:me && \
+if [ "$NODE_ENV" != "production" ]
+then
+  npm run bootstrap:nodemodules && \
+  npm run link:me
+else
+  true
+fi && \
 
 echo "OK"
 
--- scripts/update-starter-kit.sh
+++ scripts/update-starter-kit.sh
@@ -9,8 +9,8 @@ fi
 
 HOME='.'
 git pull ../lib-starter-kit
-rsync -avE --progress  ../lib-starter-kit/ $HOME --exclude='*/' --exclude='lerna.json' --exclude='.huskyrc.json' --exclude='README.md'
-rsync -avE --progress  ../lib-starter-kit/.storybook $HOME
-rsync -avE --progress  ../lib-starter-kit/scripts/* $HOME/scripts
+rsync -avEp --progress  ../lib-starter-kit/ $HOME --exclude='*/' --exclude='lerna.json' --exclude='.huskyrc.json' --exclude='README.md'
+rsync -avEp --progress  ../lib-starter-kit/.storybook $HOME
+rsync -avEp --progress  ../lib-starter-kit/scripts/* $HOME/scripts
 
 echo "\n\n\nYou need:\nnpm install && npm run bootstrap && npm run update"
