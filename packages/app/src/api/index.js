2ae74909128b7223d8a27b7018b055b721f82bb3
Igor Suvorov <hi@isuvorov.ru>
2019-12-06 22:59:14 +0200

    вроде бы работает!

--- packages/app/src/api/index.js
+++ packages/app/src/api/index.js
@@ -1,7 +1,20 @@
 import IndexApi from './IndexApi';
 
+const getTemplate = (req, res) => {
+  const str = require('fs').readFileSync(`${process.cwd()}/public/index.html`).toString();
+  return res.send(str);
+  // require('fs').readFileSync(`${process.cwd()}/packages/app/public/index.html`).toString();
+
+  return 'asdasdas';
+};
+
+
 export default function () {
   return {
+    '/': getTemplate,
+    '/sonya': getTemplate,
+    '/yukioru': getTemplate,
+
     '/api': new IndexApi(this),
     '/storage/*': async () => {
       // TODO: МАПИНГ СТАТИКИ С ПРОДА ПОСМОТРЕТЬ КАК У ХАЙДЖЕЯ
