370cd9660e72774732cdc39a2a50d3c3d5e8d048
Igor Suvorov <hi@isuvorov.ru>
2019-12-07 12:23:29 +0200

    quick fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,7 +17,7 @@ echo "OK - cra build" && \
 cd .. && \
 rm -rf public && \
 cp -R cra/build public && \
-cp -R locales public/locales && \
+# cp -R locales public/locales && \
 # node -e "const str = require('fs').readFileSync('./public/index.html').toString(); console.log(str);"
 node -e "const str = require('fs').readFileSync('./public/index.html').toString(); const code = str.split('script>')[3]; console.log(code.substr(0, code.length - 2));" > public/chunks.js && \
 node -e "const str = require('fs').readFileSync('./public/index.html').toString(); const code = str.split('<div id=\"root\"></div>')[1]; console.log(code.substr(0, code.length - '</body></html>'.length));" > public/footer.html && \
