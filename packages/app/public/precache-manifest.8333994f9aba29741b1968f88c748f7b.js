4212ec6a34f6357b8a4d23088f0cd8e722c9ec4b
sophiyabezruk <sophiya.bezruk1@yandex.ru>
2020-01-04 22:48:30 +0200

    заливаем что-то новенькое по стартапу

---
+++ packages/app/public/precache-manifest.8333994f9aba29741b1968f88c748f7b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a89dd3ab11a71e6ad959d2c3d051bef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e66e97b9cf9ed2d6bc7",
+    "url": "/static/js/2.bebc9d19.chunk.js"
+  },
+  {
+    "revision": "fde7c61e605fa75de340",
+    "url": "/static/js/main.37b5aa1a.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "48b199012d897bc237fbb429ae812a71",
+    "url": "/static/media/bg_1.48b19901.jpg"
+  },
+  {
+    "revision": "14ace77f0904b5bdfd157824c53bdbb1",
+    "url": "/static/media/bg_2.14ace77f.jpg"
+  },
+  {
+    "revision": "3cf97837524dd7445e9d1462e3c4afe2",
+    "url": "/static/media/open-iconic.3cf97837.woff"
+  },
+  {
+    "revision": "47151e87c5a8666791a91007de4962da",
+    "url": "/static/media/open-iconic.47151e87.eot"
+  },
+  {
+    "revision": "527eff645d8ead16666afb6931041279",
+    "url": "/static/media/open-iconic.527eff64.otf"
+  },
+  {
+    "revision": "93cc7aa654ae36b0828716f5ec3159cd",
+    "url": "/static/media/open-iconic.93cc7aa6.ttf"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/static/media/team-4-800x800.23007132.jpg"
+  }
+]);
