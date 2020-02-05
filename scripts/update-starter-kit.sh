7bf989784830970cce53af010281ab3d36410433
Igor Suvorov <hi@isuvorov.com>
2020-02-05 17:30:10 +0200

    feat: ignore changelog in update-stareter-kit

--- scripts/update-starter-kit.sh
+++ scripts/update-starter-kit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 HOME='.'
 git pull ../lib-starter-kit
-rsync -avEp --progress  ../lib-starter-kit/ $HOME --exclude='*/' --exclude='lerna.json' --exclude='.huskyrc.json' --exclude='README.md' --exclude='CHANGELOGz.md'
+rsync -avEp --progress  ../lib-starter-kit/ $HOME --exclude='*/' --exclude='lerna.json' --exclude='.huskyrc.json' --exclude='README.md' --exclude='CHANGELOG.md' --exclude='.env.js'
 rsync -avEp --progress  ../lib-starter-kit/.storybook $HOME
 rsync -avEp --progress  ../lib-starter-kit/scripts/* $HOME/scripts
 
