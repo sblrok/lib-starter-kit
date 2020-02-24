4dece4459f7c39f6e5bab030dc3262db9d154778
Igor Suvorov <hi@isuvorov.com>
2020-02-24 22:44:49 +0200

    feat: new cases from bg

---
+++ .lskjs.js
@@ -0,0 +1,8 @@
+module.exports = {
+  i18: {
+    url: 'https://docs.google.com/spreadsheets/d/1yqEtc7VfCZRv4I3iqSiKSq9CkSDCU3fIKa-ZayyW_ys/edit#gid=0',
+  },
+  updates: {
+    deps: ['@lskjs/.*'],
+  },
+};
