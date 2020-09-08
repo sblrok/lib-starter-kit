f2946c7ecd0e24b7fbd44ed925866a73dcdb8703
Igor Suvorov <hi@isuvorov.com>
2020-09-08 10:02:40 +0300

    chore: update experience from kit

--- scripts/build-locales.sh
+++ scripts/build-locales.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
-
-LOCALES_LINK=`node -e "console.log(require('./.lskjs.js').i18.url)"`
-LOCALES_DIST="./packages/app/i18"
-
-echo "$LOCALES_LINK => $LOCALES_DIST"
-
-rm -rf $LOCALES_DIST && \
-node node_modules/@lskjs/build-locales/bin/build-locales --locales ru,en --link $LOCALES_LINK --dist $LOCALES_DIST && \
-echo "done"
+lerna exec -- npm run i18
--- scripts/update-starter-kit.sh
+++ scripts/update-starter-kit.sh
@@ -9,12 +9,10 @@ rsync -avEp --progress  ../lib-starter-kit/ $DIRTO \
   --exclude='.lskjs.js' \
   --exclude='coverage' \
   --exclude='lerna.json' \
-  # --exclude='.huskyrc.json' \
   --exclude='README.md' \
   --exclude='CHANGELOG.md'\
   --exclude='.env.js' && \
 rsync -avEp --progress --ignore-existing ../lib-starter-kit/.lskjs.js $DIRTO
-# rsync -avEp --progress --ignore-existing ../lib-starter-kit/.huskyrc.json $DIRTO
 rsync -avEp --progress --ignore-existing ../lib-starter-kit/README.md $DIRTO
 rsync -avEp --progress --ignore-existing ../lib-starter-kit/CHANGELOG.md $DIRTO
 rsync -avEp --progress  ../lib-starter-kit/.storybook $DIRTO && \
